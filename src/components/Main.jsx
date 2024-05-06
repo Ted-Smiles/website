2435af04d6312604d579610496904047b5a79fdf
Ted Smiles <ted@smilesfamily.co.uk>
2024-05-06 19:08:48 +0100

    desltop version complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,9 +14,11 @@ const Main = () => {
             <img id="leaves" src="../src/images/D089D2A1-5D61-4EE9-8147-33BDBEAEF4DD.png" />
             <img id="flower" src="../src/images/222518DE-95E2-465B-B8EC-3A6C1C9490CC.png" />
             <div>
-                <button >
-                    <Link to="/gallery">GALLERY</Link>
-                </button>
+                <Link to="/gallery">
+                    <button >
+                        GALLERY
+                    </button>
+                </Link>
             </div>
         </div>
     )
