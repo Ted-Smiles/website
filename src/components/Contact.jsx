a0c72f3a247d82a93fcdf3aa1b0f71e76a03bbba
Ted Smiles <ted@smilesfamily.co.uk>
2024-05-18 14:51:41 +0100

    small screen changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,45 @@
 const Contact = () => {
-    return (
-        <div className="contact">
-            <div className="contact-container">
-                <div className="column">
-                    <div className="contact-title">
-                        <h1>Lorraine Penchion</h1>
-                        <h3>Watercolour Artist located in Altrincham, UK</h3>
-                    </div>
-                    <h4>Reach out to me for all your enquiries regarding sales and commissions!</h4>
-                    <br />
-                    <button>EMAIL ME</button>
-                    <p>T: +44(0)1234 567890</p>
-                    <div className="icons">
-                        <a href="https://www.instagram.com/lorrainepenchionart/" target="blank"><i class="fa fa-instagram"></i></a>
-                        <i class="fa fa-facebook-f"></i>
-                    </div>
-                </div>
-                <div className="column">
-                    <img src="../src/images/40B6CF4E-EBD9-47DC-8E72-29A1A0B65A25.jpeg" alt="" />
-                </div>
-            </div>
-        </div>
-    )
-}
+  const emailAddress = "your_email@example.com";
 
-export default Contact
+  const handleClick = () => {
+    const mailtoLink = `mailto:${emailAddress}`;
+    window.location.href = mailtoLink;
+  };
+
+  return (
+    <div className="contact">
+      <div className="contact-container">
+        <div className="column">
+          <div className="contact-title">
+            <h1>Lorraine Penchion</h1>
+            <h3>Watercolour Artist located in Altrincham, UK</h3>
+          </div>
+          <h4>
+            Reach out to me for all your enquiries regarding sales and
+            commissions!
+          </h4>
+          <br />
+          <button onClick={handleClick}>EMAIL ME</button>
+          <p>T: +44(0)1234 567890</p>
+          <div className="icons">
+            <a
+              href="https://www.instagram.com/lorrainepenchionart/"
+              target="blank"
+            >
+              <i class="fa fa-instagram"></i>
+            </a>
+            <i class="fa fa-facebook-f"></i>
+          </div>
+        </div>
+        <div className="column">
+          <img
+            src="../src/images/40B6CF4E-EBD9-47DC-8E72-29A1A0B65A25.jpeg"
+            alt="a painting of a dog sleeping in a chair"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
